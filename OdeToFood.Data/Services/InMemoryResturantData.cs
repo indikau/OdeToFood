4da578a9c86da6266ea3c0fec0a5ae0175272970
Indika Udawatta <iudawatt-cw@starbucks.com>
2019-09-26 22:26:28 -0700

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryResturantData.cs
@@ -0,0 +1,21 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryResturantData : IResturantData
+    {
+        List<Resturant> resturants;
+
+        public InMemoryResturantData()
+        {
+
+        }
+
+        public IEnumerable<Resturant> GetAll()
+        {
+            return resturants;
+        }
+    }
+}
